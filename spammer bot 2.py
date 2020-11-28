5fa3c8f258b9722ea517dfd03bb17fa46bc15fa7
IshaanDixit07 <70959343+IshaanDixit07@users.noreply.github.com>
2020-11-28 19:48:19 +0530

    Update spammer bot 2.py

--- spammer bot 2.py
+++ spammer bot 2.py
@@ -1,7 +1,8 @@
 import pyautogui
+#You may have to import this.
 
-spam = pyautogui.prompt('Enter message to spam') 
-f_o_r = 1
+spam = pyautogui.prompt('Enter message to spam') #Tell the message
+f_o_r = 1 #Let this be 1 otherwise it wont stop!!
 longitude = int(pyautogui.prompt('enter how many times(remember 2 times = 1 time!!)')) 
 
 while f_o_r < longitude: 
